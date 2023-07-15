a12fb0c68d5ce78cff63986bc257a6c782852e7d
shokaji <shokaji.mail@gmail.com>
2023-07-15 21:58:30 +0900

    ・HeadingのasobiyaロゴをSVGファイルに変更

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,9 @@
-import { Heading, Box } from '@chakra-ui/react'
+import { Flex, Image } from '@chakra-ui/react'
 
-const MyHeading = ({ children }) => (
-  <Box width="100%" textAlign="center" py="280" ml="24" pb="4">
-    <Heading as="h1" size="3xl">
-      {children}
-    </Heading>
-  </Box>
+const MyHeading = () => (
+  <Flex width="60%" justifyContent="center" ml="24">
+    <Image src="/images/asobiya.svg" alt="asobiya" mt="-190px" />
+  </Flex>
 )
 
 export default MyHeading

6a43dbbde1638e8ca433b64a9eb1e0a28c6b05de
shokaji <shokaji.mail@gmail.com>
2023-07-15 23:43:37 +0900

    ・ダークモード時のHeaderのロゴがホワイトになるように修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Image, useColorModeValue } from '@chakra-ui/react'
 
-const Header = () => (
-  <Flex width="65%" justifyContent="center" ml="24">
-    <Image src="/images/asobiya.svg" alt="asobiya" mt="-200px" />
-  </Flex>
-)
+const Header = () => {
+  const filter = useColorModeValue('none', 'invert(1)')
+
+  return (
+    <Flex width="65%" justifyContent="center" ml="24">
+      <Image
+        src="/images/asobiya.svg"
+        alt="asobiya"
+        mt="-200px"
+        style={{ filter }}
+      />
+    </Flex>
+  )
+}
 
 export default Header

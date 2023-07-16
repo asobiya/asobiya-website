f61281f526cbe0dcb84f71df6637045381b69b8b
shokaji <shokaji.mail@gmail.com>
2023-07-16 12:16:55 +0900

    Footerをシンプルなデザインに修正にしました

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,31 +2,20 @@ import {
   Box,
   chakra,
   Container,
-  Link,
+  Image,
   Stack,
-  Text,
   useColorModeValue,
   VisuallyHidden,
-  useTheme,
 } from '@chakra-ui/react'
 import { FaInstagram, FaTwitter, FaYoutube, FaDiscord } from 'react-icons/fa'
-import { ReactNode } from 'react'
 
-const SocialButton = ({
-  children,
-  label,
-  href,
-}: {
-  children: ReactNode
-  label: string
-  href: string
-}) => {
+const SocialButton = ({ children, label, href }) => {
   return (
     <chakra.button
       bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
       rounded={'full'}
-      w={8}
-      h={8}
+      w={14}
+      h={14}
       cursor={'pointer'}
       as={'a'}
       href={href}
@@ -45,9 +34,6 @@ const SocialButton = ({
 }
 
 export default function SmallCentered() {
-  const theme = useTheme()
-  const font = theme.fonts.heading
-
   return (
     <Box
       bg={useColorModeValue('gray.50', 'gray.900')}
@@ -56,78 +42,49 @@ export default function SmallCentered() {
       <Container
         as={Stack}
         maxW={'6xl'}
-        pt={5}
-        pb={3}
-        spacing={4}
+        pt={1}
+        pb={8}
         direction={{ base: 'column', md: 'row' }}
+        spacing={4}
         justify={{ base: 'center', md: 'space-between' }}
-        align={'center'}
+        align={{ base: 'center', md: 'center' }}
       >
+        <Image
+          boxSize="200px"
+          objectFit="cover"
+          src="/images/asobiya.svg"
+          alt="asobiya"
+        />
+
         <Stack direction={'row'} spacing={6}>
-          <Link href={'/about'} fontSize="sm">
-            About
-          </Link>
-          <Link href={'/members'} fontSize="sm">
-            Members
-          </Link>
-          <Link href={'https://discord.gg/asobiya'} fontSize="sm">
-            Discord
-          </Link>
-          <Link href={'https://forms.gle/GYXm5wmJfSbbERwTA'} fontSize="sm">
-            Contact
-          </Link>
+          <SocialButton
+            label={'Discord'}
+            href={'https://discord.com/invite/R4BFAgTVYx'}
+          >
+            <FaDiscord fontSize="1.5em" />
+          </SocialButton>
+
+          <SocialButton
+            label={'Twitter'}
+            href={'https://twitter.com/asobiya_dao'}
+          >
+            <FaTwitter fontSize="1.5em" />
+          </SocialButton>
+          <SocialButton
+            label={'Instagram'}
+            href={'https://www.instagram.com/asobiya_dao/'}
+          >
+            <FaInstagram fontSize="1.5em" />
+          </SocialButton>
+
+          <SocialButton
+            label={'YouTube'}
+            href={'https://www.youtube.com/channel/UCjGmgBPlLTOKSsXiqToZIKA'}
+          >
+            <FaYoutube fontSize="1.5em" />
+          </SocialButton>
         </Stack>
       </Container>
-
-      <Box
-        borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}
-      >
-        <Container
-          as={Stack}
-          maxW={'6xl'}
-          pt={1}
-          pb={8}
-          direction={{ base: 'column', md: 'row' }}
-          spacing={4}
-          justify={{ base: 'center', md: 'space-between' }}
-          align={{ base: 'center', md: 'center' }}
-        >
-          <Text fontFamily={font} fontSize="sm">
-            asobiya
-          </Text>
-
-          <Stack direction={'row'} spacing={6}>
-            <SocialButton
-              label={'Discord'}
-              href={'https://discord.com/invite/R4BFAgTVYx'}
-            >
-              <FaDiscord />
-            </SocialButton>
-
-            <SocialButton
-              label={'Twitter'}
-              href={'https://twitter.com/asobiya_dao'}
-            >
-              <FaTwitter />
-            </SocialButton>
-            <SocialButton
-              label={'Instagram'}
-              href={'https://www.instagram.com/asobiya_dao/'}
-            >
-              <FaInstagram />
-            </SocialButton>
-
-            <SocialButton
-              label={'YouTube'}
-              href={'https://www.youtube.com/channel/UCjGmgBPlLTOKSsXiqToZIKA'}
-            >
-              <FaYoutube />
-            </SocialButton>
-          </Stack>
-        </Container>
-      </Box>
     </Box>
   )
 }
